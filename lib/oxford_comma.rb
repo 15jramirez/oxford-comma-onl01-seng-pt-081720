e3007841f2430cf302d3f3ed550b85bab9300143
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-08-27 07:22:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+if array.length == 1 
+  array.join
+  elsif array.length == 2
+     array.join(" and ")
+ else
+   last_index << array.last
+   array.pop
+   array.join (", ")
+ end
 end
